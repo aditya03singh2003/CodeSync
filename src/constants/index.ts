ea9fa1754aff204f6b508407a2f1617089a2f79b
aditya03singh2003 <aditya.2201093cs@iiitbh.ac.in>
2025-04-16 20:54:10 +0530

    4th commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -89,6 +89,13 @@ export const CODING_QUESTIONS: CodeQuestion[] = [
         
     }
 }`,
+      cpp: `class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Write your solution here
+        
+    }
+};`
     },
     constraints: [
       "2 ≤ nums.length ≤ 104",
@@ -126,6 +133,13 @@ export const CODING_QUESTIONS: CodeQuestion[] = [
         
     }
 }`,
+      cpp: `class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        // Write your solution here
+        
+    }
+};`
     },
   },
   {
@@ -160,6 +174,13 @@ export const CODING_QUESTIONS: CodeQuestion[] = [
         
     }
 }`,
+      cpp: `class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Write your solution here
+        
+    }
+};`
     },
   },
 ];
@@ -168,6 +189,7 @@ export const LANGUAGES = [
   { id: "javascript", name: "JavaScript", icon: "/javascript.png" },
   { id: "python", name: "Python", icon: "/python.png" },
   { id: "java", name: "Java", icon: "/java.png" },
+  { id: "cpp", name: "C++", icon: "/cpp.png" },
 ] as const;
 
 export interface CodeQuestion {
@@ -183,6 +205,7 @@ export interface CodeQuestion {
     javascript: string;
     python: string;
     java: string;
+    cpp: string;
   };
   constraints?: string[];
 }
